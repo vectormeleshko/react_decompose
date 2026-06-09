b8e3bec5058039385a9ee5efea396f7245bd4a79
Viktor Meleshko <vectormeleshko@gmail.com>
2026-06-09 21:33:57 +0300

    changed Site Name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,9 @@ import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 function Header() {
-
   return (
     <header className="header">
-      <h1 className="header__title">Site name</h1>
+      <h1 className="header__title">Site Name</h1>
       <Navigation />
     </header>
   );
